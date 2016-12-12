b630f4744cbd25ca165f478f544f525e84753126
Nikhil Kanoor <nk352@afsconnect2.njit.edu>
2016-12-11 22:55:07 -0500

    commit

---
+++ code/core/get.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<a href="test_get.php?subject=PHP&web=W3schools.com">Test $GET</a>
+<?php 
+echo "Study " . $_GET['subject'] . " at " . $_GET['web'];
+/*another way to collect form data after submitting an HTML form*/
+?>
+
+</body>
+</html>
